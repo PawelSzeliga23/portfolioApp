d5b96629c802601be6b3b590ec77c4b5d6b3b24c
s27030 <s27030@pjwstk.edu.pl>
2025-03-31 12:52:40 +0200

    profile&home twiks

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,21 @@
-import logo from '../assets/ps-high-resolution-logo-transparent.png';
+import profil from '../assets/profil.png';
+import { motion } from 'framer-motion';
 
 const Home = () => {
     return (
-        <section className='fade-in relative w-full h-auto sm:h-screen mx-auto px-5 sm:px-10 max-sm:pt-20 '>
+        <section className='fade-in relative w-full h-auto sm:h-screen mx-auto px-5 sm:px-10 max-sm:pt-20'>
             <div className='max-w-5xl w-full mx-auto px-5 flex flex-col sm:flex-row items-center justify-center h-full gap-10 sm:gap-20'>
                 <div className='flex flex-col items-center sm:items-start justify-center h-full'>
                     <div>
                         <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold'> Hi I'm</h1>
                         <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold text-lime-500'> Paweł Szeliga</h1>
                         <p className='text-base sm:text-lg md:text-xl mt-3 text-center sm:text-left'>
-                            I am a Computer Science student at the Polish-Japanese Academy of Information Technology.
-                            I am constantly improving my skills through academic and personal projects.
+                        I really enjoy computer science and am currently studying it while gaining experience through my personal and academic projects.
                         </p>
                     </div>
                     <div>
                         <h1 className='text-2xl sm:text-3xl font-bold mt-5'>Skills :</h1>
-                        <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 mt-3'>
+                        <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-3'>
                             <li className='flex items-center gap-1'><i className="devicon-react-plain colored text-2xl"></i> React</li>
                             <li className='flex items-center gap-1'><i className="devicon-typescript-plain colored text-2xl"></i> TypeScript</li>
                             <li className='flex items-center gap-1'><i className="devicon-javascript-plain colored text-2xl"></i> JavaScript</li>
@@ -29,7 +29,7 @@ const Home = () => {
                             <li className='flex items-center gap-1'><i className="devicon-java-plain colored text-2xl"></i> Java</li>
                         </ul>
                     </div>
-                    <div className='flex flex-col sm:flex-row items-center justify-center gap-10 mt-5'>
+                    <div className='flex flex-col sm:flex-col md:flex-row lg:flex-row items-center justify-center gap-10 mt-5'>
                         <div>
                             <a
                                 href='/public/Paweł_Szeliga_CV.pdf'
@@ -38,8 +38,8 @@ const Home = () => {
                             >
                                 Download CV
                                 <span className="material-symbols-outlined">
-                                download
-                            </span>
+                                    download
+                                </span>
                             </a>
                         </div>
                         <div className='flex gap-4'>
@@ -60,7 +60,17 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <img src={logo} alt='avatar' className='w-60 sm:w-60 md:w-80 lg:w-96 rounded-full' />
+                <div className="relative w-60 sm:w-60 md:w-80 lg:w-96">
+                    <motion.div
+                        className="w-60 sm:w-60 md:w-80 lg:w-96 h-60 sm:h-60 md:h-80 lg:h-96 rounded-full border-4 border-dashed border-lime-500"
+                        animate={{ rotate: 360 }}
+                        transition={{ repeat: Infinity, duration: 30, ease: "linear" }}
+                    />
+
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <img src={profil} alt='avatar' className='w-60 sm:w-60 md:w-80 lg:w-96 rounded-full' />
+                    </div>
+                </div>
             </div>
         </section>
     );
